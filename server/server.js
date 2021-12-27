af47e5c8e9167f4e0b8d136a8a8c324be590c791
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-12-27 17:12:38 -0500

    working on auth

--- server/server.js
+++ server/server.js
@@ -9,46 +9,47 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // AUTH AND SESSION START
-// const cookieSession = require('cookie-session');
-// const passport = require('passport');
-// require('./passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./passport');
 
-// app.use(
-//   cookieSession({
-//     name: 'github-auth-session',
-//     keys: ['key1', 'key2'],
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(
+  cookieSession({
+    name: 'github-auth-session',
+    keys: ['key1', 'key2'],
+  })
+);
 
-// app.get('/auth/error', (req, res) => res.send('Unknown Error'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.get(
-//   '/auth/github',
-//   passport.authenticate('github', { scope: ['user:email'] })
-// );
+app.get('/auth/error', (req, res) => res.send('Unknown Error'));
 
-// app.get(
-//   '/auth/github/callback',
-//   passport.authenticate('github', { failureRedirect: '/auth/error' }),
-//   function (req, res) {
-//     res.redirect('/');
-//   }
-// );
-// // AUTH AND SESSION END
+app.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+);
 
-// app.get('/', (req, res) => {
-//   res.send(`Hello world`);
-// });
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/auth/error' }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+// AUTH AND SESSION END
 
-// // LOG IN AND LOG OUT
-// app.get('/login', (req, res) => res.redirect('/auth/github'))
-// app.get('/logout', (req, res) => {
-//   req.session = null;
-//   req.logout();
-//   res.redirect('/');
-// })
+app.get('/', (req, res) => {
+  res.send(`Hello world`);
+});
+
+// LOG IN AND LOG OUT
+app.get('/login', (req, res) => res.redirect('/auth/github'))
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+})
 
 
 // ERROR HANDLING
