2be69b3989f98f766ac5b8b4bf68ed9e43200b05
Grace Yun <graceyunn@gmail.com>
2021-12-30 19:07:36 -0500

    Debugging deployment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,10 +2,8 @@ const authControllers = {};
 
 authControllers.isLoggedIn = (req, res, next) => {
   if (req.user) {
-    console.log('USER IS LOGGED IN: ', req.user)
     return next();
   } else {
-    console.log('NOT AUTHORIZED REDIRECTING!')
     res.status(401).redirect('/');
   }
 }
